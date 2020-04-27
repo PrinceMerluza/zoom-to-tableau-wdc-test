4d8efc8759375c55d3a503a1c05e5d623da46c6e
hero2bash <zero.bash@gmail.com>
2020-04-27 19:58:26 +0800

    Remove getToken

--- main.js
+++ main.js
@@ -98,7 +98,7 @@ $(document).ready(function() {
     } 
     
     if(queryParams.code){
-        getToken(queryParams.code);
+        // getToken(queryParams.code);
     } else {
         window.location.href = "https://zoom.us/oauth/authorize?response_type=code&"
             + "client_id=" + clientId
