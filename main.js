6c28d5d4d8f40ba65d543a06d5e5ccad335f59b8
hero2bash <zero.bash@gmail.com>
2020-04-27 15:38:56 +0800

    Add token getting

--- main.js
+++ main.js
@@ -1,11 +1,43 @@
-var redirectUri = "https://princemerluza.github.io/zoom-to-tableau-wdc-test/";
+var redirectUri = "https://princemerluza.github.io/zoom-to-tableau-wdc-test/index.html";
 var clientId = "3U9rD5THSbucRLn5_W2ynQ";
+var token = '';
+
+function getToken(code){
+    $.ajax('https://zoom.us/oauth/token?'
+            + 'grant_type=authorization_code&' 
+            + 'code=' + code
+            + 'redirect_uri=' + redirectUri,
+        {
+            headers: {
+                Authorization: 'Basic M1U5ckQ1VEhTYnVjUkxuNV9XMnluUTpsSzBTYUtGR09vUm5McmpXR1dud2RGTVF4dk1GMENYNg=='
+            },
+            complete: function(data){
+                console.log(data);
+                token = data.access_token;
+            }
+        }
+    )
+}
 
 $(document).ready(function() {
-    if(window.location.href.includes("code")){
-        console.log("Yep");
+    // Get Query Parameters
+    var queryParamsString = window.location.href.includes('?') ? 
+            window.location.href.split("?")[1] : null;
+    var queryParams = {};
+
+    if(queryParamsString){
+        queryParamsString.split('&').forEach(function(q){
+            var pair = q.split('=');
+            queryParams[pair[0]] = pair[1];
+        });
+        console.log(queryParams);
+    } 
+        
+    if(queryParams.code){
+        testAPI(queryParams.code);
     } else {
-        window.location.href = "https://zoom.us/oauth/authorize?response_type=code&" + "client_id=" + clientId + 
-        "&redirect_uri=" + redirectUri;
+        window.location.href = "https://zoom.us/oauth/authorize?response_type=code&"
+            + "client_id=" + clientId
+            + "&redirect_uri=" + redirectUri;
     }
 });
