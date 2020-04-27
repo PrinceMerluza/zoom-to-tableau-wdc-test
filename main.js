de1ad393f74c4f2af83d4ff45dba2cb42549cdf6
hero2bash <zero.bash@gmail.com>
2020-04-27 17:07:40 +0800

    Remove token function

--- main.js
+++ main.js
@@ -2,24 +2,6 @@ var redirectUri = "https://princemerluza.github.io/zoom-to-tableau-wdc-test/inde
 var clientId = "3U9rD5THSbucRLn5_W2ynQ";
 var token = '';
 
-function getToken(code){
-    $.ajax('https://zoom.us/oauth/token?'
-            + 'grant_type=authorization_code&' 
-            + 'code=' + code + '&'
-            + 'redirect_uri=' + redirectUri,
-        {
-            headers: {
-                Authorization: 'Basic M1U5ckQ1VEhTYnVjUkxuNV9XMnluUTpsSzBTYUtGR09vUm5McmpXR1dud2RGTVF4dk1GMENYNg=='
-            },
-            method: 'POST',
-            complete: function(data){
-                console.log(data);
-                token = data.access_token;
-            }
-        }
-    )
-}
-
 $(document).ready(function() {
     // Get Query Parameters
     var queryParamsString = window.location.href.includes('?') ? 
@@ -35,7 +17,8 @@ $(document).ready(function() {
     } 
         
     if(queryParams.code){
-        getToken(queryParams.code);
+        console.log(queryParams.code)
+        // getToken(queryParams.code);
     } else {
         window.location.href = "https://zoom.us/oauth/authorize?response_type=code&"
             + "client_id=" + clientId
