14ec3f8fa157478ccb3fb68383c1bb1828c228cd
hero2bash <zero.bash@gmail.com>
2020-04-28 16:00:23 +0800

    Add lambda function for getting meetings

--- main.js
+++ main.js
@@ -45,14 +45,11 @@ var clientId = "3U9rD5THSbucRLn5_W2ynQ";
 
     myConnector.getData = function(table, doneCallback){
         var token = tableau.password;
-        throw new Error(token);
-        $.ajax('https://api.zoom.us/v2/users/me/meetings', {
+
+        $.ajax('https://ri64kb0pda.execute-api.ap-southeast-1.amazonaws.com/getZoomMeetings?token=' + token, {
             method: 'GET',
-            headers: {
-                Authorization: 'Bearer ' + token
-            },
             complete: function(data, status){
-                var meetings = data.responseJSON.meetings;
+                var meetings = JSON.parse(data.responseText);
                 var tableData = [];
 
                 // NOTE: Doesn' support paging
